de2e6e4a6eaf559de75bc964c3db7e3a723c9ca5
alalUDDIN123 <alalu75666@gmail.com>
2023-08-30 04:31:20 -1200

    object

---
+++ 10_object.js
@@ -0,0 +1,5 @@
+// Defination : In JavaScript, objects are a fundamental data structure used to store and organize data. 
+//  They consist of key-value pairs, where the keys are strings (or Symbols) and the values can be of any data type,
+//  including other objects or functions.
+
+// How to create  an object ?
